d76cd10faaa46a76c14f685a7a270971db4379e8
Yohan <Edward@Edward-PC>
2019-05-17 11:14:57 -0500

    Entidad user

---
+++ src/com/pgr3/mr_bid/model/CreditCard.java
@@ -0,0 +1,39 @@
+package com.pgr3.mr_bid.model;
+
+import java.time.LocalDate;
+
+public class CreditCard {
+	
+	private LocalDate expirationDate;
+	private String cardholderName;
+	private String cardNumber;
+	private String securityCode;
+	
+	public CreditCard(LocalDate expirationDate, String cardholderName, String cardNumber, String securityCode) {
+		this.expirationDate = expirationDate;
+		this.cardholderName = cardholderName;
+		this.cardNumber = cardNumber;
+		this.securityCode = securityCode;
+	}
+	
+	public CreditCard () {
+		
+	}
+
+	public LocalDate getExpirationDate() {
+		return expirationDate;
+	}
+
+	public String getCardholderName() {
+		return cardholderName;
+	}
+
+	public String getCardNumber() {
+		return cardNumber;
+	}
+
+	public String getSecurityCode() {
+		return securityCode;
+	}	
+
+}
--- src/com/pgr3/mr_bid/model/User.java
+++ src/com/pgr3/mr_bid/model/User.java
@@ -1,5 +1,54 @@
 package com.pgr3.mr_bid.model;
 
+import java.time.LocalDate;
+
 public class User {
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private LocalDate birthDate;
+	private boolean isFemale;
+	private CreditCard creditCard;
+	
+	public User(String firstName, String lastName, String email, String password, LocalDate birthDate, boolean isFemale,
+			CreditCard creditCard) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.birthDate = birthDate;
+		this.isFemale = isFemale;
+		this.creditCard = creditCard;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public boolean isFemale() {
+		return isFemale;
+	}
+
+	public CreditCard getCreditCard() {
+		return creditCard;
+	}
 
 }
