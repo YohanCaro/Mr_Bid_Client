f0690b8538c24076064fc919198b72146711bc48
danie <danie@DESKTOP-1M5N7PD>
2019-05-26 20:57:23 -0500

    Se elimino panel sobrante

--- test/view/AddProduct.java
+++ test/view/AddProduct.java
@@ -1,11 +1,9 @@
 package view;
 
-import com.prg3.mr_bid.view.JFrameMainAddProduc;
-
 public class AddProduct {
 
 	
 	public static void main(String[] args) {
-		JFrameMainAddProduc jFrameMainAddProduc = new JFrameMainAddProduc();
+//		JFrameMainAddProduc jFrameMainAddProduc = new JFrameMainAddProduc();
 	}
 }
