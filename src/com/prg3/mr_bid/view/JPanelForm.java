9dce7da25f66984739600cf3ccbfff73d5538f22
danie <danie@DESKTOP-1M5N7PD>
2019-05-26 19:28:20 -0500

    cambios

--- src/com/prg3/mr_bid/view/JPanelForm.java
+++ src/com/prg3/mr_bid/view/JPanelForm.java
@@ -13,12 +13,14 @@ import java.util.Map;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JSpinner;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -29,15 +31,20 @@ import javax.swing.text.DefaultFormatter;
 
 import com.prg3.mr_bid.controller.Commands;
 import com.prg3.mr_bid.controller.UserController;
+import com.toedter.calendar.JDateChooser;
 
 public class JPanelForm extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	private JLabel jLabelName, jLabelImage;
 	private JButton jBLogin, jButton;
-	private JTextField jTextFieldName, jTextFieldEmail, jTextFieldPasswor, jTextFieldid, jTextFieldBirthdate, jTextFieldgenero, jTextFieldCaptchat ;
+	private JTextField jTextFieldName, jTextFieldEmail, jTextFieldPasswor, jTextFieldid, jTextFieldBirthdate,
+			jTextFieldgenero, jTextFieldCaptchat;
 	private JComboBox jComboBox;
 	private JPanelRobotCenter jPanelRobotCenter;
+	private JDateChooser jDateChooser;
+	private JRadioButton jRadioButton, jRadioButton2;
+	private ButtonGroup buttonGroup;
 
 	public JPanelForm(UserController controller) {
 		this.jPanelRobotCenter = new JPanelRobotCenter();
@@ -52,13 +59,13 @@ public class JPanelForm extends JPanel {
 		this.setBackground(Color.WHITE);
 
 		createLabel("Nombre", 20, 20);
-		createTextField("name", 150, 20,jTextFieldName);
+		createTextField("name", 150, 20, jTextFieldName);
 
 		createLabel("EMAIL", 20, 70);
-		createTextField("email", 150, 70,jTextFieldEmail);
+		createTextField("email", 150, 70, jTextFieldEmail);
 
 		createLabel("Passwor", 20, 120);
-		createTextField("Password", 150, 120,jTextFieldPasswor);
+		createTextField("Password", 150, 120, jTextFieldPasswor);
 
 		createLabel("Tipo de Documento", 20, 170);
 		jComboBox = new JComboBox<String>();
@@ -73,26 +80,40 @@ public class JPanelForm extends JPanel {
 		jComboBox.addItem("PASAPORTE");
 
 		createLabel("Numero Documento", 20, 220);
-		createTextField("Numero Documento", 150, 220,jTextFieldid);
-		
+		createTextField("Numero Documento", 150, 220, jTextFieldid);
+
 		createLabel("Fecha de Nacimiento", 20, 270);
-		createTextField("Fecha de Nacimiento", 150, 270,jTextFieldBirthdate);
-		
+		jDateChooser = new JDateChooser();
+		jDateChooser.setBounds(150, 280, 250, 30);
+		add(jDateChooser);
+
 		createLabel("Genero", 20, 320);
-		createTextField("Genero", 150, 320,jTextFieldgenero);
-		
-		
+		// createTextField("Genero", 150, 320, jTextFieldgenero);
+
+		jRadioButton = new JRadioButton("FEMALE");
+		jRadioButton.setBounds(150, 330, 100, 30);
+		jRadioButton.setSelected(true);
+		add(jRadioButton);
+
+		jRadioButton2 = new JRadioButton("MALE");
+		jRadioButton2.setBounds(250, 330, 100, 30);
+		add(jRadioButton2);
+
+		buttonGroup = new ButtonGroup();
+		buttonGroup.add(jRadioButton);
+		buttonGroup.add(jRadioButton2);
+
 		jButton = new JButton("Add Tarjeta");
 		jButton.setActionCommand(Commands.SHOW_ADD_CREDIT_CARD.name());
 		jButton.addActionListener(controller);
 		jButton.setBounds(150, 370, 250, 40);
 		add(jButton);
-		
+
 		createLabel("CAPTCHAT", 20, 420);
-		createTextField("CAPTCHAT", 150, 420,jTextFieldCaptchat);
-		
+		createTextField("CAPTCHAT", 150, 420, jTextFieldCaptchat);
+
 		jBLogin = new JButton("Registrar");
-		jBLogin.setBounds(20, 470, 450, 40);
+		jBLogin.setBounds(150, 480, 250, 40);
 		add(jBLogin);
 
 	}
@@ -106,7 +127,7 @@ public class JPanelForm extends JPanel {
 		return jLabelName;
 
 	}
-	
+
 	public JLabel createLabel2(String name, int x, int y) {
 		jLabelImage.add(jPanelRobotCenter);
 		jLabelImage.setBounds(x, y, 200, 50);
@@ -116,7 +137,6 @@ public class JPanelForm extends JPanel {
 		return jLabelImage;
 
 	}
-
 
 	public JTextField createTextField(String name, int x, int y, JTextField atribute) {
 		atribute = new JTextField(name);
