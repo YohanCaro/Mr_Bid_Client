1e4e5fa828aaa3ad39ab78a39de3fb986192dd6c
Yohan <yohancaro7@gmail.com>
2019-06-01 12:27:49 -0500

    Cambio en el runner

--- src/com/prg3/mr_bid/runner/Run.java
+++ src/com/prg3/mr_bid/runner/Run.java
@@ -1,13 +1,43 @@
 package com.prg3.mr_bid.runner;
 
-import com.prg3.mr_bid.controller.UserController;
+import java.io.IOException;
+import java.net.UnknownHostException;
 
+import com.prg3.mr_bid.communication.Client;
+import com.prg3.mr_bid.controller.UserController;
+import com.prg3.mr_bid.utilities.Constants;
+
+/**
+ * Clase Run - Inicia la aplicaión del cliente
+ *
+ * @version 1.2 - 1/06/2019
+ */
 public class Run {
 
+	/**
+	 * Main
+	 * @param args (ip, port) (localHost 12345)
+	 */
 	public static void main(String[] args) {
-
-		new UserController();
-
+		if (args.length == 2) {
+			try {
+				Constants.ip = args[0];
+				Constants.port = Integer.parseInt(args[1]);
+				Client.getInstanceOf();
+				UserController.getInstanceOf();
+			} catch (UnknownHostException e) {
+				System.out.println("No se ha encontrado al servidor");
+			} 
+			catch (NumberFormatException e) {
+				System.out.println("El parametro del puerto debe ser numerico! (EJM: 12345)");
+			} 
+			catch (IOException e) {
+				System.out.println("No se ha podido establecer conexión con el servidor");
+			}
+		} else {
+			System.out.println("Debe ingresar la ip y el puerto");
+		}
+		
 	}
 
 }
