50620c043738d757c13d2949dcec0230185af033
danie <danie@DESKTOP-1M5N7PD>
2019-06-03 20:03:59 -0500

    NUEVAS CONSTANTES

--- src/com/prg3/mr_bid/model/entity/ConstantsBid.java
+++ src/com/prg3/mr_bid/model/entity/ConstantsBid.java
@@ -23,6 +23,35 @@ public class ConstantsBid {
 	public static final String T_SUMMARY = "T_SUMMARY";
 	public static final String T_CREATEAUCTIONS = "T_CREATEAUCTIONS";
 	public static final String T_PROFILE = "T_PROFILE";
-	public static final String T_CONFIGURATION = "T_CONFIGURATION";
+	public static final String T_CONFIGURATION = "T_CONFIGURATION";	
+	public static final String T_KIND = "T_KIND";
+	public static final String T_STATE = "T_STATE";
+	public static final String T_TIMEREAMING = "T_TIMEREAMING";
+	public static final String T_ENTER = "T_ENTER";
+	public static final String T_TIME = "T_TIME";
+	public static final String T_FINISH = "FINISH";
+	public static final String T_CURRENTVALUE = "T_CURRENTVALUE";
+	public static final String T_VALUEBID = "T_VALUEBID";
+	public static final String T_BID = "T_BID";
+	public static final String T_OFFEREDBY = "T_OFFEREDBY";
+	public static final String T_NUMBEROFPUBLICATIONS = "T_NUMBEROFPUBLICATIONS";
+	public static final String T_PARTICIPANTS = "T_PARTICIPANTS";
+	public static final String T_MYAUCTIONS = "T_MYAUCTIONS";
+	public static final String T_MYPARTICIPATIONS = "T_MYPARTICIPATIONS";
+	public static final String T_NAME = "T_NAME";
+	public static final String T_EMAIL = "T_EMAIL";
+	public static final String T_PASSWORD = "T_PASSWORD";
+	public static final String T_DOCUMENTTYPE = "T_DOCUMENTTYPE";
+	public static final String T_BIRTHDATE = "T_BIRTHDATE";
+	public static final String T_ADD_CARD = "T_ADD_CARD";
+	public static final String T_EXPIRATION = "T_EXPIRATION";
+	public static final String T_EXPIRATION_DATE = "T_EXPIRATION_DATE";
+	public static final String T_NAMEOFOWNER = " T_NAMEOFOWNER";
+	public static final String T_CARDNUMBER = "T_CARDNUMBER";
+	public static final String T_CODE = "T_CODE";
+	public static final String T_ACCEPT = "T_ACCEPT";
+	
+	
 
 }
+
